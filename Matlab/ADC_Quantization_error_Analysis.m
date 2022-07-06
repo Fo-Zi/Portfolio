1b8b0ccf839d70ea693ab29da8a23acbcf06f3da
Facu-Z <85625336+Facu-Z@users.noreply.github.com>
2022-07-06 19:47:44 -0300

    Update ADC_Quantization_error_Analysis.m

--- Matlab/ADC_Quantization_error_Analysis.m
+++ Matlab/ADC_Quantization_error_Analysis.m
@@ -1,15 +1,11 @@
 % SCRIPT - ADC Quantization error analysis
 % v1.0
 
-% BRIEFING %
-% This script allows you analize the discrete quantization error signal, its correlation
-% with the input signal, and its spectrum. This can be useful if you're trying to determine 
-% if quantization is affecting your implementation.
-
 % COMMENTS IN USAGE %
-% -The representation of the spectrum can be improved by sampling more than 
-% the 2 periods i choose for this example. I chose two just to visualize 
-% better the quantized signal and its associated quantizion error.
+% The representation of the spectrum of the discrete quantization error can
+% be improved by sampling more than the 2 periods that i choose for this
+% example. I chose 2 only to visualize better the quantized signal and its
+% associated quantizion error.
 
 % ----------Config ADC---------- % 
 Nbits = 12;
@@ -19,7 +15,7 @@ LSB = Vref/(2^Nbits);
 % ----------Signals to be sampled---------- %
 f1 = 50*10^3 ;  % Signal freq of 50kHz
 Vcc1 = 1.65;  
-A1 = 0.4;   % Sin amplitude
+A1 = 1;   % Sin amplitude
 y1 = @(t) Vcc1 + A1*sin(2*pi*f1*t) ;    % Sinusoidal signal example with Vcc value !=0
 
 % ----------Sampling interval---------- %
@@ -40,8 +36,11 @@ codebook = 0:LSB/2:Vref;            % rounding down (floor)
 % ----------Quantization error sequence---------- %
 EqY = Y - qY;  % Discrete sequence: quantiz. error
 
+% ---------- Auto-correlation quantization error seq. ---------- %
+R_EqY_EqY = ifft(fft(EqY.*conj(fft(EqY))));
+
 % ----------Circular Cross-correlation of input signal(Y) and Quantiz. err. sequence (EqY) ---------- %
-C_Y_EqY = ifft(fft(Y.*conj(fft(qY))));
+C_Y_EqY = ifft(fft(Y.*conj(fft(EqY))));
 
 % ----------Single-Sided Amplitude Spectrum of EqY---------- %
 L = length(EqY);             % Length of signal
@@ -50,7 +49,6 @@ P2 = abs(Eq1f/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = fs*(0:(L/2))/L;
-
 
 % ----------Plotting---------- %
 figure(1)
@@ -75,14 +73,21 @@ axis([0 t2 min(EqY) max(EqY)])
 grid
 
 figure(2)
-subplot(2,1,1)
-plot(t(2:end),C_Y_EqY(2:end),'o')
-title('Circular Cross-correlation of input signal(Y) and Quantiz. err. sequence (qY) ')
+subplot(3,1,1)
+plot(t(1:end),C_Y_EqY(1:end),'o')
+title('Circular Cross-correlation of input signal(Y) and Quantiz. err. sequence (EqY) ')
 xlabel('Time (t)')
-ylabel('C_Y_EqY(t)')
+ylabel('C_Y_EqY (t)')
 grid
 
-subplot(2,1,2)
+subplot(3,1,2)
+plot(t(1:end),C_Y_EqY(1:end),'o')
+title('Auto-correlation of Quantiz. err. seq (EqY)')
+xlabel('Time (t)')
+ylabel('R_EqY_EqY (t)')
+grid
+
+subplot(3,1,3)
 plot(f,P1) 
 title('Single-Sided Amplitude Spectrum of e[n]')
 xlabel('f (Hz)')
