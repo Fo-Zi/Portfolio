bdf218babeab4aa417a0976db520c13665a307ac
Facu-Z <85625336+Facu-Z@users.noreply.github.com>
2022-07-06 22:44:10 -0300

    Update

--- Matlab/IIR_Filtering_Analysis.m
+++ Matlab/IIR_Filtering_Analysis.m
@@ -1,42 +1,77 @@
 % SCRIPT - IIR filtering analysis %
 
 fs = 1*10^6;
-%z = tf('z',ts);
 ALPHA = single(0.9999);
-%Hi = (1-ALPHA)/(1-ALPHA*(z^-1));
 
-b = 1- ALPHA;
+% 3rd order IIR filter -> 3 IIR single pole cascaded %
 a1 = [1 -ALPHA];
 a2 = [1 -ALPHA];
 a3 = [1 -ALPHA]; 
 a = conv(a3,conv(a1,a2));
+b = 1- ALPHA;
 
-c = ALPHA;
+% Complementary 3rd order IIR filter -> 3 IIR single pole cascaded %
 d1 = [1 -(1-ALPHA)];
 d2 = [1 -(1-ALPHA)];
 d3 = [1 -(1-ALPHA)]; 
 d = conv(a3,conv(a1,a2));
+c = ALPHA;
+
+% Complementary 1st order IIR filter %
+e = [1 -(1-ALPHA)];
+f = ALPHA;
 
 n=1000;
-[h1,w1] = freqz(b,a,n,fs);
-[h2,w2] = freqz(c,d,n,fs);
+[h1,f1] = freqz(b,a,n,fs);
+[h2,f2] = freqz(c,d,n,fs);
+[h3,f3] = freqz(f,e,n,fs);
 
 figure(1)
 subplot(2,1,1)
-plot(w1,20*log10(abs(h1)))
+plot(f1,20*log10(abs(h1)))
 hold on
+plot(f2,20*log10(abs(h2)))
+title('Frequency response 3rd order IIR filter')
+xlabel('Frequency [Hz]')
+ylabel('Magnitude [dB]')
+legend('3rd order filter','3rd order complementary filter');
 grid
-ax = gca;
-ax.YLim = [-100 40];
-ax.XTick = 0:.5:2;
-xlabel('Normalized Frequency (\times\pi rad/sample)')
-ylabel('Magnitude (dB)')
 
 subplot(2,1,2)
-plot(w2,20*log10(abs(h2)))
+plot(f1,angle(h1))
+hold on
+plot(f2,angle(h2))
+xlabel('Frequency [Hz]')
+ylabel('Phase [deg]')
+legend('3rd order filter','3rd order complementary filter');
 grid
-ax = gca;
-ax.YLim = [-100 40];
-ax.XTick = 0:.5:2;
-xlabel('Normalized Frequency (\times\pi rad/sample)')
-ylabel('Magnitude (dB)')
+
+%{
+figure(2)
+subplot(2,1,1)
+plot(f2,20*log10(abs(h2)))
+title('Frequency response complementary 3rd order IIR filter')
+xlabel('Frequency [Hz]')
+ylabel('Magnitude [dB]')
+grid
+
+subplot(2,1,2)
+plot(f2,angle(h2))
+xlabel('Frequency [Hz]')
+ylabel('Phase [deg]')
+grid
+%}
+
+figure(3)
+subplot(2,1,1)
+plot(f3,20*log10(abs(h3)))
+title('Frequency response complementary 1st order IIR filter')
+xlabel('Frequency [Hz]')
+ylabel('Magnitude [dB]')
+grid
+
+subplot(2,1,2)
+plot(f3,angle(h3))
+xlabel('Frequency [Hz]')
+ylabel('Phase [deg]')
+grid
